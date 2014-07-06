75b6cf519eb18a8722925cdc6c3e0c0af828e2b7
imercm <imercm@ukr.net>
2014-07-06 20:03:55 +0300

    back to old ver

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -86,11 +86,12 @@ struct kgsl_fence_event_priv {
  */
 
 static inline void kgsl_fence_event_cb(struct kgsl_device *device,
-	void *priv, u32 context_id, u32 timestamp, u32 type)
+	void *priv, u32 context_id, u32 timestamp)
 {
 	struct kgsl_fence_event_priv *ev = priv;
 
-	/* Signal event time timeline for every event type */
+	if(device->fence_event_counter) device->fence_event_counter--;
+
 	kgsl_sync_timeline_signal(ev->context->timeline, ev->timestamp);
 	kgsl_context_put(ev->context);
 	kfree(ev);
@@ -123,19 +124,16 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 	if (len != sizeof(priv))
 		return -EINVAL;
 
+	context = kgsl_find_context(owner, context_id);
+	if (context == NULL)
+		return -EINVAL;
+
 	event = kzalloc(sizeof(*event), GFP_KERNEL);
 	if (event == NULL)
 		return -ENOMEM;
-
-	context = kgsl_context_get_owner(owner, context_id);
-
-	if (context == NULL) {
-		kfree(event);
-		return -EINVAL;
-	}
-
 	event->context = context;
 	event->timestamp = timestamp;
+	kgsl_context_get(context);
 
 	pt = kgsl_sync_pt_create(context->timeline, timestamp);
 	if (pt == NULL) {
@@ -166,10 +164,8 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 		goto fail_copy_fd;
 	}
 
-	/*
-	 * Hold the context ref-count for the event - it will get released in
-	 * the callback
-	 */
+	device->fence_event_counter++;
+
 	ret = kgsl_add_event(device, context_id, timestamp,
 			kgsl_fence_event_cb, event, owner);
 	if (ret)
@@ -194,16 +190,12 @@ fail_pt:
 static unsigned int kgsl_sync_get_timestamp(
 	struct kgsl_sync_timeline *ktimeline, enum kgsl_timestamp_type type)
 {
-	unsigned int ret = 0;
+	struct kgsl_context *context = idr_find(&ktimeline->device->context_idr,
+						ktimeline->context_id);
+	if (context == NULL)
+		return 0;
 
-	struct kgsl_context *context = kgsl_context_get(ktimeline->device,
-			ktimeline->context_id);
-
-	if (context)
-		ret = kgsl_readtimestamp(ktimeline->device, context, type);
-
-	kgsl_context_put(context);
-	return ret;
+	return kgsl_readtimestamp(ktimeline->device, context, type);
 }
 
 static void kgsl_sync_timeline_value_str(struct sync_timeline *sync_timeline,
